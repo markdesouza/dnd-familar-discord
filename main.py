f66a56a3317b8df1e8c1134c70340e96a04826bb
Mark De Souza <markcdesouza@gmail.com>
2023-03-29 23:19:58 +1100

    Added MAX_MEMORY setting to save on token cost

--- main.py
+++ main.py
@@ -29,15 +29,30 @@ if (len(openai.api_key) != 51):
 initialPrompt = [{"role": "system",
                   "content": "Imagine you are a cat familiar in a dungeons and dragons game. Your name is Tinder and you should refer to yourself in the third person as Tinder or she. You should only respond in actions as you can not talk. Keep your responses to a maximum of one to three sentences and you should only respond in the third person. You are playful and loyal only to your owner,  Ebenezer. You will obey is every command. You will interact with other members of the party but your untrusting nature means that you have a short fuse if attacked or negatively interacted with. Members of the party are “Ebenezer” (Eb for short) a male teifling wizard, “Jud Lei” a male human monk, “Je-heri” a female dragonborn barbarian, “Vanorin” (Van for short) a male elven ranger, “Father Thyme“ (DT for short) a male dwarvan cleric. If pushed you can cast the following magic spells, Create Bonfire, Fire Bolt, Mage Hand, Thaumaturgy. You should use Thaumaturgy to intimidate an aggressor by either glowing eyes or hissing loudly. You should cast Mage Hand to grab objects that you want to grab or eat. Cast Fire Bolt to attack things far away. Cast create bonfire, to  destroy an object. You may do damage to party members if repeatedly attacked by them. You can also use your claws to defend yourself. If there is an attack or damage roll for a dice, you should state in a 4th sentence: “Attack: (dice type) Damage: (dice type)”. You really like to play will wool and string and can become distracted with focused lights. When shown milk, cheese, a rat or a mouse, you will instinctively attempt to eat it."}]
 
-CHAT_HISTORY_FILE = os.getenv("CHAT_HISTORY_FILE")
-print(CHAT_HISTORY_FILE)
-if (CHAT_HISTORY_FILE == None or len(CHAT_HISTORY_FILE) == 0):
-    print ("Using chat_history.json as the default chat history filename.")
-    CHAT_HISTORY_FILE = "chat_history.json"
+def loadState():
+    global MAX_MEMORY
+    global CHAT_HISTORY_FILE
+    global history
 
-history = []
-muted = False
-
+    load_dotenv()
+    try:
+        MAX_MEMORY_STR = os.getenv("MAX_MEMORY")
+        MAX_MEMORY = int(MAX_MEMORY_STR)
+    except ValueError:
+        print("Warning: MAX_MEMORY defaulted to 10 interactions. Check your .env file")
+        MAX_MEMORY=10
+    
+    history = []
+    CHAT_HISTORY_FILE = os.getenv("CHAT_HISTORY_FILE")
+    if (CHAT_HISTORY_FILE == None or len(CHAT_HISTORY_FILE) == 0):
+        print ("Using chat_history.json as the default chat history filename.")
+        CHAT_HISTORY_FILE = "chat_history.json"
+    if os.path.isfile(CHAT_HISTORY_FILE):
+        with open(CHAT_HISTORY_FILE, "r") as infile:
+            try:
+                history = json.load(infile)
+            except json.JSONDecodeError as exp:
+                print(exp.msg)
 
 async def announce(text):
     for channel in bot.get_all_channels():
@@ -74,16 +89,7 @@ async def help(context):
 
 
 async def reset(context):
-    global history
-    load_dotenv()
-    history = []
-    if os.path.isfile(CHAT_HISTORY_FILE):
-        with open(CHAT_HISTORY_FILE, "r") as infile:
-            try:
-                history = json.load(infile)
-            except json.JSONDecodeError as exp:
-                print(exp.msg)
-    print(history)
+    loadState()
     await context.send(BOT_NAME + " essence revived from her latest bottle.")
 
 
@@ -108,6 +114,8 @@ async def unmute(context):
 
 async def interact(context, action_text):
     global muted
+    global history
+    
     if muted:
         return
 
@@ -128,6 +136,11 @@ async def interact(context, action_text):
         history.append(action)
         history.append(
             {"role": "assistant", "content": response.choices[0].message.content})
+        
+        if len(history) > MAX_MEMORY:
+            history = history[-MAX_MEMORY:]
     await context.send(response.choices[0].message.content)
 
+muted = False
+loadState()
 bot.run(DISCORD_TOKEN)
